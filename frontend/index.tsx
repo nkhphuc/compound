3a0781379792107a207e2d4fb4b2290a3ac73bcc
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2025-06-28 09:48:08 +0700

    Remove tailwind cdn.

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import i18n from 'i18next';
 import { initReactI18next, I18nextProvider } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import './index.css';
 
 // --- BEGIN EMBEDDED TRANSLATIONS ---
 const enTranslations = {
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./constants.ts"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
